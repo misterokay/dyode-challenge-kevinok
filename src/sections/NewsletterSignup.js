8daa54c3fad45db6f54b625fea8c0508c9780a4a
Kevin Ok <kevin.ok87@gmail.com>
2022-03-08 18:15:54 -0800

    newsletter section

--- src/sections/NewsletterSignup.js
+++ src/sections/NewsletterSignup.js
@@ -1,27 +1,46 @@
 import React from 'react';
+import newsletterSvg from '../images/newsletter.svg';
 
 class NewsletterSignup extends React.Component {
-    state = { email: '' };
+    state = { 
+        email: '',
+        formSubmitted: false
+    };
 
     onFormSubmit = (event) => {
         event.preventDefault();
 
-        // this.props.onSubmit(this.state.email);
+        // insert formSubmit function
+
+        this.setState({ formSubmitted: true });
     }
 
     render() {
         return (
             <div className="NewsletterSignupSection">
                 <div className="newsletter-wrap">
-                    <div className="newsletter-heading">Sign Up & Stay Connected</div>
+                    <div className="newsletter-heading"><img src={newsletterSvg} />Sign Up &amp; Stay Connected</div>
                     <form onSubmit={this.onFormSubmit} className="">
-                        <div className="">
-                            <label htmlFor=""></label>
-                            <input
-                                type="email"
-                                value={this.state.email}
-                                onChange={(e) => this.setState({ email: e.target.value })}
-                            />
+                        <div className="form-content">
+                            <p>
+                                Sign up for the newsletter and get 20% off! Gain access to exclusive offers and be the first to know when new stuff drops!
+                            </p>
+                            {this.state.formSubmitted ?
+                                <div className="form-post-submit">
+                                    Thanks for signing up!
+                                </div>
+                                :
+                                <div className="form-pre-submit">
+                                    <input
+                                        placeholder="Enter Your Email Address"
+                                        className="email-field"
+                                        type="email"
+                                        value={this.state.email}
+                                        onChange={(e) => this.setState({ email: e.target.value })}
+                                    />
+                                    <input className="submit-button" type="submit" value="SUBSCRIBE" />
+                                </div>
+                            }
                         </div>
                     </form>
                 </div>
